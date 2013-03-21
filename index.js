924b18d68291dceb30471e71f9a6f36af0c70a4f
Alex Ford <Alex@CodeTunnel.com>
2013-03-21 07:33:15 -0600

    Created listener that will return both socket.io-client.js and shotgun.client.js client libraries.

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var io = require('socket.io');
+var io = require('socket.io'),
+    fs = require('fs'),
+    path = require('path');
 
 exports.attach = function (server, shell) {
+    var oldListeners = server.listeners('request');
+    server.removeAllListeners('request');
+    server.on('request', function (req, res) {
+        if (req.url === '/shotgun/shotgun.client.js') {
+            try {
+                fs.readFile(path.join(__dirname, '/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js'), function (err, socketIoClient) {
+                    fs.readFile(path.join(__dirname, '/client/shotgun.client.js'), function (err, shotgunClient) {
+                        res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                        res.end(socketIoClient + '\n\n' + shotgunClient);
+                    });
+                });
+            } catch (e) {}
+        }
+        else {
+            for (var i = 0, l = oldListeners.length; i < l; i++) {
+                oldListeners[i].call(server, req, res);
+            }
+        }
+    });
     io.listen(server)
         .of('/shotgun')
         .on('connection', function (socket) {
             socket.on('execute', function (cmdStr, context, options) {
-            var result = shell.execute(cmdStr, context, options);
-            socket.emit('result', result);
+                var result = shell.execute(cmdStr, context, options);
+                socket.emit('result', result);
+            });
         });
-    });
 };
