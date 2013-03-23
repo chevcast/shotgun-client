2192ab86403aae5fd88d674941c9ee79bd0cbc79
Alex Ford <Alex@CodeTunnel.com>
2013-03-23 15:37:17 -0600

    Updated jQuery adapter and client shell to offer more flexibility. Also updated README to reflect API changes.

--- README.md
+++ README.md
@@ -62,9 +62,30 @@ If you'd like to see an example of shotgun-client running in an [Express](http:/
         </body>
     </html>
 
-The included jQuery adapter is designed to get you up and running quickly. Just call `.shotgunConsole()` on any element and it will be instantly transformed into a simple console that communicates directly, in realtime with your shotgun shell on the server. This is more for the user who wants some sort of admin interface for their website.
+The included jQuery adapter is designed to get you up and running quickly. Just call `.shotgunConsole()` on any element and it will be instantly transformed into a simple console that communicates directly, in realtime with your shotgun shell on the server. This is more for the user who wants some sort of admin interface for their website. If you need more control you can override the `handleResult()` function and do work with the shotgun shell result manually.
 
-If you need something more customized then you can forgo using the jQuery wrapper and use the client shell directly.
+    $('body').shotgunConsole(function (result, $display, $console) {
+
+        //   result - the object returned by the shotgun shell.
+        // $display - the element to write text to.
+        // $console - the element you called .shotgunConsole() on. In this example it is 'body'.
+
+        if (result.clearDisplay) $display.html('');
+        if (result.exit) {
+            $console.slideUp('fast', function () {
+                $console.empty();
+            });
+        }
+        for (var count = 0; count < result.lines.length; count++) {
+            var line = result.lines[count];
+            $display.append(line.text.replace(/  /g, ' &nbsp;') + '<br />');
+            $this.scrollTop($this[0].scrollHeight);
+        };
+    });
+
+The code in the example callback function is the same code that is executed if you provide no callback.
+
+If you need something even more customized or simply don't want to use jQuery then it's still really simple. Just create and use the client shell directly.
 
 **Client w/out jQuery**
 
@@ -91,3 +112,13 @@ If you need something more customized then you can forgo using the jQuery wrappe
         <body>
         </body>
     </html>
+
+Note that you have access to `result.context` but you don't have to worry about passing it back into `client.execute`; the shotgun client script will do this for you. However, if for some reason you do need to manipulate the context then you can pass it back like this:
+
+
+    var client = new shotgun.Client(),
+        customContext = {}; // Usually result.context.
+    client.execute('help', customContext, function (result) {
+        console.log(result);
+        // result is the exact same object returned by shell.execute() on the server.
+    });
--- client/jquery.shotgunConsole.js
+++ client/jquery.shotgunConsole.js
@@ -1,38 +1,46 @@
 (function ($) {
-    $.fn.shotgunConsole = function () {
-        var $this = this,
-            client = new shotgun.Client(),
-            $display = $('<div>')
-                .appendTo($this);
-            $cliContainer = $('<div>')
-                .html('&gt;&nbsp;')
-                .appendTo($this),
-            $cli = $('<input>')
-                .attr('type', 'text')
-                .css({
-                    backgroundColor: 'transparent',
-                    color: $this.css('color'),
-                    fontSize: $this.css('font-size'),
-                    width: '75%',
-                    border: 'none',
-                    outline: 'none',
-                    marginTop: '5px'
-                })
-                .appendTo($cliContainer);
+
+    $.fn.shotgunConsole = function (handleResult) {
+        var $console = this,
+            client = new shotgun.Client();
+
+        var $display = $('<div>').appendTo($console);
+
+        var $cliContainer = $('<div>').html('&gt;&nbsp;').appendTo($console);
+
+        var $cli = $('<input>')
+            .attr('type', 'text')
+            .attr('autofocus', 'autofocus')
+            .css({
+                backgroundColor: 'transparent',
+                color: $console.css('color'),
+                fontSize: $console.css('font-size'),
+                width: '75%',
+                border: 'none',
+                outline: 'none',
+                marginTop: '20px'
+            })
+            .appendTo($cliContainer)
+            .focus();
 
         $cli.keypress(function (e) {
             if (e.which == 13) {
                 client.execute($cli.val(), function (result) {
-                    if (result.clearDisplay) $display.html('');
-                    if (result.exit) {
-                        window.open('','_self','');
-                        window.close();
+                    if (handleResult)
+                        handleResult(result, $display, $console);
+                    else {
+                        if (result.clearDisplay) $display.html('');
+                        if (result.exit) {
+                            $console.slideUp('fast', function () {
+                                $console.empty();
+                            });
+                        }
+                        for (var count = 0; count < result.lines.length; count++) {
+                            var line = result.lines[count];
+                            $display.append(line.text.replace(/  /g, ' &nbsp;') + '<br />');
+                            $console.scrollTop($console[0].scrollHeight);
+                        };
                     }
-                    for (var count = 0; count < result.lines.length; count++) {
-                        var line = result.lines[count];
-                        $display.append(line.text.replace(/  /g, ' &nbsp;') + '<br />');
-                        $this.scrollTop($this[0].scrollHeight);
-                    };
                 });
                 $cli.val('');
             }
