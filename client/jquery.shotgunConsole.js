c8766b98456a7658550f91a72cb95ee31d18dd57
Alex Ford <Alex@CodeTunnel.com>
2013-04-10 16:07:23 -0600

    Added jquery.coolType to jquery.shotgunConsole to give an interesting "typing" effect to the console.

--- client/jquery.shotgunConsole.js
+++ client/jquery.shotgunConsole.js
@@ -41,21 +41,45 @@
                     }
                     if (callback)
                         callback(result, $display, $console);
-                    for (var count = 0; count < result.lines.length; count++) {
-                        var line = result.lines[count],
-                            $line = $('<div>')
-                            text = line.text.replace(/  /g, ' &nbsp;') + '<br />';
-                        switch (line.type) {
-                            case 'warn':
-                                text = 'warning: ' + text;
-                                break;
-                            case 'error':
-                                text = 'error: ' + text;
-                                break;
+
+                    var index = 0;
+                    (function typeLine() {
+                        if (index < result.lines.length) {
+                            var line = result.lines[index++],
+                                $line = $('<div>'),
+                                text = line.text.replace(/(  +)/g, function (match) {
+                                    return new Array(match.length + 1).join('&nbsp;');
+                                }).replace(/(\r\n|\r|\n)/, '');
+
+                            text = text.length > 0 ? text : '&nbsp;';
+
+                            switch (line.type) {
+                                case 'warn':
+                                    text = 'warning: ' + text;
+                                    break;
+                                case 'error':
+                                    text = 'error: ' + text;
+                                    break;
+                            }
+
+                            $line.appendTo($display);
+                            if ('coolType' in $.fn && text.length > 0) {
+                                typeOptions = {
+                                    typeSpeed: 0,
+                                    delayBeforeType: 0,
+                                    delayAfterType: 0,
+                                    onComplete: typeLine
+                                };
+                                $line.coolType(text, typeOptions);
+                            }
+                            else {
+                                $line.html(text);
+                                typeLine();
+                            }
+
+                            $console.scrollTop($console[0].scrollHeight);
                         }
-                        $line.html(text).appendTo($display);
-                        $console.scrollTop($console[0].scrollHeight);
-                    };
+                    })();
                 });
                 $cli.val('');
             }
