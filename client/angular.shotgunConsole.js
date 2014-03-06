6d7d49a9f02219fcbf063051c962e682cc93589d
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-03-05 17:42:01 -0700

    Finished Angular adapter for JQuery plugin.

--- client/angular.shotgunConsole.js
+++ client/angular.shotgunConsole.js
@@ -1,34 +1,28 @@
-angular.module('shotgun', [])
-    .directive('shotgunConsole', function () {
-        return {
-            restrict: 'EACM',
-            replace: true,
-            template: '<div class="console-container"><div class="console-display"></div><div style="margin-top: 10px;" class="console-cli-container"><span class="console-cli-text">&gt;&nbsp;</span><input class="console-cli" type="text" autofocus="autofocus" class="single-line cli" style="font-family: inherit; font-size: inherit; font-style: inherit; font-variant: inherit; font-weight: inherit; line-height: inherit; color: inherit; background-color: transparent; width: 75%; border: none; outline: none; resize: none;"></div></div>'
-            scope: {
-                cliText: '&gt;&nbsp;'
-            },
-            link: function (scope, elem, attrs) {
-                var input = $($elem.children()[0]),
-                    button = $($elem.children()[1]);
-                $scope.$watch('value', function (val) {
-                    input.val(val);
-                });
-                input.autocomplete({
-                    source: $scope.source(),
-                    select: function (event, ui) {
-                        $scope.$apply(function () {
-                            $scope.value = ui.item.value;
-                        });
-                    },
-                    close: function () {
-                        input.autocomplete('option', 'minLength', 9999);
-                    },
-                    minLength: 9999
-                });
-                button.click(function () {
-                    input.autocomplete('option', 'minLength', 0);
-                    input.autocomplete('search', input.val());
-                });
+// Place the directive inside a custom "shotgun" angular module/namespace.
+angular.module('shotgun', []).directive('shotgunConsole', function () {
+    return {
+        // Allow users to use the directive any way they choose.
+        restrict: 'EACM',
+        link: function (scope, element, attrs) {
+            
+            // Simply invoke the JQuery adapter and get reference to the client shell.
+            var clientShell = element.shotgunConsole(attrs);
+
+            // Don't attach the shell to the scope if noScope is true.
+            if (!attrs.noScope) {
+
+                // Create a shotgunShells namespace on our scope object.
+                if (!scope.hasOwnProperty('shotgunShells')) scope.shotgunShells = {};
+
+                // Attach the shell instance to a hash of instantiated shells, using its
+                // namespace as the property name.
+                scope.shotgunShells[clientShell.settings.namespace] = clientShell;
+
             }
-        };
-    });
+
+            // If an "execute" property was included then pass the value to the shell.
+            if (attrs.execute) clientShell.execute(attrs.execute);
+
+        }
+    };
+});
